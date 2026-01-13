63fd5d2f6a92b54c1436ed76eef108ab08bd81f0
yuch <52803366+chilya-coder@users.noreply.github.com>
2026-01-13 18:37:36 +0000

    Time: 109 ms (80.15%), Space: 57.2 MB (62.09%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,8 +2,8 @@ class RandomizedSet:
 
 
     def __init__(self):
-        self.list = list()
-        self.dict = dict()
+        self.list = list() #self.list = [val0, val1, val2] each value will have coresponded index same as in dict
+        self.dict = dict() #self.dict = {val0:index0, val1:index1, val2:index2}
         
 
     def insert(self, val: int) -> bool:
@@ -11,7 +11,7 @@ class RandomizedSet:
             return False
         else:
             self.list.append(val)
-            self.dict[val] = len(self.list) - 1
+            self.dict[val] = len(self.list) - 1 #we add element to the dict with the same index from list
 
     def remove(self, val: int) -> bool:
         #self.dict = {1:0, 2:1, 3:2}
@@ -24,20 +24,20 @@ class RandomizedSet:
 
         #self.dict = {1:0, 3:1}
         if val in self.dict:
-            #self.list.remove(val) this is a mistake since removing from list will traverse full list O(n)
+            #self.list.remove(val) is impossible by complexity constraint O(n)
             lastElement = self.list[-1]
             indexToReplace = self.dict[val]
 
-            self.list[indexToReplace] = lastElement
-            self.dict[lastElement] = indexToReplace
-            self.list.pop(-1)
+            self.list[indexToReplace] = lastElement #overwrite the needed val memory cell with the last element
+            self.dict[lastElement] = indexToReplace #important! updated index in the dict as well
+            self.list.pop(-1) # finally remove element from list and dict
             self.dict.pop(val)
             return True
         else:
             return False
 
     def getRandom(self) -> int:
-        return random.choice(self.list)
+        return random.choice(self.list) # return random value from list without a need to convert keys of dict to list
         
 
 
