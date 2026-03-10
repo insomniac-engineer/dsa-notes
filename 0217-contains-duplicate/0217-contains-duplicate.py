fc92ad8529d63d30b0d4bafe879d4edb1d13d4bd
yuch <52803366+chilya-coder@users.noreply.github.com>
2026-03-10 00:10:16 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -13,7 +13,9 @@ class Solution:
         #         fastP += 1
         # return False
 
-        number_frequency = Counter(nums)
-        for n in number_frequency.values():
-            if n != 1: return True
-        return False
+        # number_frequency = Counter(nums)
+        # for n in number_frequency.values():
+        #     if n != 1: return True
+        # return False
+
+        return len(nums) != len(set(nums))
