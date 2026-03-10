b04867b72d603aa4534c956fbdc71969867da424
yuch <52803366+chilya-coder@users.noreply.github.com>
2026-03-10 00:09:36 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,14 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort() #[1,1,2,3]
+        # nums.sort() #[1,1,2,3]
 
-        slowP = 0
-        fastP = 1
+        # slowP = 0
+        # fastP = 1
 
-        while fastP < len(nums):
-            if nums[slowP] == nums[fastP]:
-                return True
-            else:
-                slowP += 1
-                fastP += 1
+        # while fastP < len(nums):
+        #     if nums[slowP] == nums[fastP]:
+        #         return True
+        #     else:
+        #         slowP += 1
+        #         fastP += 1
+        # return False
+
+        number_frequency = Counter(nums)
+        for n in number_frequency.values():
+            if n != 1: return True
         return False
