ac201353416307a1d8ab7d41fa044b83255eabfd
chilya-coder <yuliya.chimyrys@gmail.com>
2026-03-11 11:46:08 +0000

    Refactor removeDuplicates and containsDuplicate methods; add isHappy and containsNearbyDuplicate solutions with documentation

--- problems/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ problems/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,11 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        j = 1
+        slowP = 0
+        fastP = 1
 
-        while j < len(nums):
-            if nums[i] != nums[j]:
-                i += 1
-                nums[i] = nums[j]
-            j += 1
-        return i + 1
+        while fastP < len(nums):
+            if nums[slowP] != nums[fastP]:
+                slowP += 1
+                nums[slowP] = nums[fastP]
+            fastP += 1
+        return slowP + 1
