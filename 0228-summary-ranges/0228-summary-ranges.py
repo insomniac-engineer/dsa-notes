72db8fadd5389c14ce42fa7155288c3d3f8a044f
yuch <52803366+chilya-coder@users.noreply.github.com>
2026-03-16 23:33:36 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,27 +1,22 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        #slow and fast pointer approach
-        slow = 0
-        fast = 1
         result = []
+        i = 0
 
-        startIndex = 0
+        while i < len(nums):
+            # fix the start position
+            start = nums[i]
+            
+            # move while next value == +1
+            while i + 1 < len(nums) and nums[i] + 1 == nums[i + 1]:
+                i += 1
+            
+            # if we didn't make a move aka there is no value with value+1
+            if start == nums[i]:
+                result.append(f"{start}")
+            else: # if we moved and found the sequence values
+                result.append(f"{start}->{nums[i]}")
+            
+            i += 1
         
-        while fast < len(nums):
-            if nums[slow] + 1 != nums[fast]:
-                if nums[slow] - 1 != nums[slow - 1]:
-                    result.append(f"{nums[slow]}")
-                else:
-                    result.append(f"{nums[startIndex]}->{nums[slow]}")
-                startIndex = slow + 1
-            slow += 1
-            fast += 1
-        
-        if startIndex != len(nums):
-            if nums[startIndex] == nums[fast - 1]:
-                result.append(f"{nums[startIndex]}")
-            else:
-                result.append(f"{nums[startIndex]}->{nums[fast - 1]}")
         return result
-
-        
