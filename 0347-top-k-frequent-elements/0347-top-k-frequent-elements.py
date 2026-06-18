619a82fc471f5b5e743b24876230737a41b41bd8
yuchi <52803366+chilya-coder@users.noreply.github.com>
2026-06-18 23:04:29 +0100

    Time: 2 ms (91.46%), Space: 22.8 MB (61.3%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,4 +5,19 @@ class Solution:
         #     my_dict[n] += 1
         my_dict = Counter(nums)
         # O(NlogK)
-        return heapq.nlargest(k, my_dict.keys(), key = my_dict.get)
+        # return heapq.nlargest(k, my_dict.keys(), key = my_dict.get)
+
+        heap = []
+
+        for key in my_dict.keys():
+            freq = my_dict[key]
+            heapq.heappush(heap, (freq, key))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        print(heap)
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
