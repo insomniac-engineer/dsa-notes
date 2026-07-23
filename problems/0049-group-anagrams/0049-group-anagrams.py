83592a87f769173a58986123b10b8529da2a98b2
yuchi <52803366+chilya-coder@users.noreply.github.com>
2026-07-23 21:30:43 +0100

    Enhance comments on time and space complexity
    Added time and space complexity comments for clarity.

--- problems/0049-group-anagrams/0049-group-anagrams.py
+++ problems/0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # if i ask for a key that doesn't exist - return []
+        # Time Complexity: O (n * k logk)
+        # Time Space: O (n * k)
+
+
+        # creates empty list value if key doesn't exist (KeyError)
         my_dict = defaultdict(list)
 
         for s in strs:
-            # we cant use sorted(s) because it returns list (mutable)
-            # list can't be stored as key in dict
-            # extra memory allocation
-            #sorted_s = ('').join(sorted(s))
+            # key in hash table MUST be hashable (immutable)
+            # when we sort string it returns list (mutable)
+            
             sorted_s = tuple(sorted(s))
+            # alternative: sorted_s = ('').join(sorted(s))
             my_dict[sorted_s].append(s)
         return list (my_dict.values())
