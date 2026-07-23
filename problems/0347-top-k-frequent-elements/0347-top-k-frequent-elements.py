af5e639fc6ebfc4748b558b49f415b05e8a9b690
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-07-23 23:33:18 +0100

    Refactor topKFrequent method for clarity
    Refactor topKFrequent method to simplify heap operations and improve readability.

--- problems/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ problems/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,23 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # my_dict = defaultdict(int)
-        # for n in nums:
-        #     my_dict[n] += 1
+        # Space Complexity: O(n)
+        # Time Complexity: O(n log k)
+        
         my_dict = Counter(nums)
-        # O(NlogK)
-        # return heapq.nlargest(k, my_dict.keys(), key = my_dict.get)
 
         heap = []
 
-        for key in my_dict.keys():
-            freq = my_dict[key]
-            heapq.heappush(heap, (freq, key))
-
+        for val, freq in my_dict.items():
+            heapq.heappush(heap, (freq, val))
+            
+            # Sift Down takes O(log k)
             if len(heap) > k:
                 heapq.heappop(heap)
 
-        print(heap)
-        res = []
-        for i in range(k):
-            res.append(heapq.heappop(heap)[1])
-        return res
+        return [val for _, val in heap]
