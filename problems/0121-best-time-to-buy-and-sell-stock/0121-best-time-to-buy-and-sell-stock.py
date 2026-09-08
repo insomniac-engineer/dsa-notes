c8e1e94d4a77d5c42833c1eabeb3997ef56a8c99
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-08 21:44:41 +0100

    Optimize maxProfit method for stock prices
    Refactor profit calculation to use buyPrice variable.

--- problems/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ problems/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        minBuy = None
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        buyPrice = prices[0]
         profit = 0
 
         for i in prices:
-            if minBuy is None or i < minBuy:
-                minBuy = i
-            if i - minBuy > profit:
-                profit = i - minBuy
-
+            buyPrice = min(buyPrice, i)
+            profit = max(profit, i - buyPrice)
         return profit
