cc6fd8e35dbaf63b2107d7811d90693aaaa99e80
chilya-coder <yuliya.chimyrys@gmail.com>
2026-09-16 22:55:43 +0100

    Refactor checkInclusion method for improved clarity and performance

--- problems/0567-permutation-in-string/0567-permutation-in-string.py
+++ problems/0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,17 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # Time Complexity: O(n)
+        # Time Complexity: O(n) - check comments as we reduced it from O(26 * n)
         # Space Complexity: O(1)
-        freq_map = defaultdict(int)
+        # permutation -> we care about chars frequency (dict)
+
+        pattern = Counter(s1)
+        seen = defaultdict(int)
+        len_s1 = len(s1)
         l = 0
-        pattern_map = Counter(s1)
+        need, have = len(pattern), 0
         for r in range(len(s2)):
-            freq_map[s2[r]] += 1
-            while r - l + 1 > len(s1):
-                freq_map[s2[l]] -= 1
-                if freq_map[s2[l]] == 0:
-                    del freq_map[s2[l]]
+            seen[s2[r]] += 1
+            # match happens only if we have enough frequency (from need) for each key
+            if pattern[s2[r]] == seen[s2[r]]:
+                have += 1
+            while r - l + 1 > len_s1:
+                if pattern[s2[l]] == seen[s2[l]]:
+                    have -= 1
+                seen[s2[l]] -= 1
+                if seen[s2[l]] == 0:
+                    del seen[s2[l]]
                 l += 1
-            if pattern_map == freq_map:
+            # # takes O(n * 26)
+            # if pattern == seen:
+            #     return True
+            # instead of comparing entire dict we compare counters
+            if have == need:
                 return True
         return False
